97c3bf953ad06da681387b3e555a34f2a1e54e89
satoshikemboi <satoshikemboi@gmail.com>
2026-01-21 16:24:36 +0300

    done

--- src/pages/SignUp.jsx
+++ src/pages/SignUp.jsx
@@ -16,7 +16,7 @@ function SignUp() {
     }
 
     try {
-      const res = await fetch("http://localhost:5000/api/users/signup", {
+      const res = await fetch("https://remocoin.onrender.com/api/users/signup", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
