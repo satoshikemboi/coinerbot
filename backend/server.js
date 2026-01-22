84b9b3e21469f943eb8491097464145a46fbebaf
satoshikemboi <satoshikemboi@gmail.com>
2026-01-22 11:08:58 +0300

    deploying on vercel

--- backend/server.js
+++ backend/server.js
@@ -19,7 +19,8 @@ app.use(cors({
   origin: [
     "http://localhost:5173",
     "http://127.0.0.1:5173",
-    "https://remocoin.netlify.app"
+    "https://remocoin.netlify.app",
+    "https://remocoin.vercel.app"
   ],
   credentials: true
 }));
