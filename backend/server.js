50a3846b3476a3917d66281f3ed1cc1b81331f7a
satoshikemboi <satoshikemboi@gmail.com>
2026-02-03 18:28:48 +0300

    added a new origin in the server

--- backend/server.js
+++ backend/server.js
@@ -15,7 +15,7 @@ app.use(cors({
     "http://localhost:5173",
     "http://127.0.0.1:5173",
     "https://coinerbot.netlify.app",
-    "https://coinerbot.vercel.app",
+    "https://coinerbot.site",
     "https://remocoin.vercel.app"
   ],
   credentials: true
